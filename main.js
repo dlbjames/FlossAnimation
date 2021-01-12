059d2b728cec50d55e1dda76cfd0a9d87e089251
dlbjames <dljames@uw.edu>
2021-01-12 15:07:25 -0800

    Focus canvas on page load

--- main.js
+++ main.js
@@ -9,7 +9,8 @@ ASSET_MANAGER.queueDownload("./scottPilgrim.png");
 ASSET_MANAGER.downloadAll(function () {
 	var canvas = document.getElementById('game');
 	var ctx = canvas.getContext('2d');
-
+	
+	canvas.focus();
 	gameEngine.init(ctx);
 	gameEngine.addEntity(new Animation(gameEngine));
 	gameEngine.start();
